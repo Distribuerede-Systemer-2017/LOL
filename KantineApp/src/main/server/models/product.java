63941300d82fd7d9814699c81c194dc5f53d42dd
Anna Nguyen <AnnaNguyen@Annas-MacBook-Air.local>
2017-10-17 09:41:18 +0200

    getters og setters

--- KantineApp/src/main/server/models/product.java
+++ KantineApp/src/main/server/models/product.java
@@ -1,4 +1,33 @@
 package main.server.models;
 
 public class product {
+
+    private int id;
+    private String productName;
+    private String productPrice;
+
+    public int getId() {
+        return id;
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
 }
